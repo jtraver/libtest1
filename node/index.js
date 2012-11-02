d0aa51292142127a1acd59bffde20bdab9823fca
John Traver <jtraver@yahoo-inc.com>
2012-11-02 11:06:10 -0700

    oops

--- node/index.js
+++ node/index.js
@@ -1,6 +1,5 @@
 module.exports = {
-
     configure: function (c) {
-        console.log('you configured your lib with ' + JSON.stringify(c, undefined, 4);
+        console.log('you configured your lib with ' + JSON.stringify(c, undefined, 4));
     }
 };
